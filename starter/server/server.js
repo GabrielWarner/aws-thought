64661231148803b91f2e4fcd868ff58e8d752164
GabrielWarner <gabe0412@uw.edu>
2022-09-23 19:25:42 -0700

    image

--- starter/server/server.js
+++ starter/server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const userRoutes = require('./routes/user-routes');
+const imageRoutes = require('./routes/image-upload');
+// app.use(require('./routes'));
+app.use('/api/', userRoutes);
+app.use('/api/', imageRoutes); // add this route for image upload
 
 // express middleware, used to be bodyparser
 app.use(express.json());
@@ -12,8 +16,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-// app.use(require('./routes'));
-app.use('/api/', userRoutes);
+
 
 // Start the API server
 app.listen(PORT, () =>
---
+++ starter/server/utils/params-config.js
@@ -0,0 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
+
+const params = (fileName) => {
+    const myFile = fileName.originalname.split('.');
+    const fileType = myFile[myFile.length - 1];
+  
+    const imageParams = {
+      // Replace the <My_Bucket_Name> with the name of your own S3 bucket
+      Bucket: 'user-images-aeb90b8f-907b-4e1e-b892-615c2161015b',
+      Key: `${uuidv4()}.${fileType}`,
+      Body: fileName.buffer,
+    };
+  
+    return imageParams;
+  };
+
+  module.exports = params;
