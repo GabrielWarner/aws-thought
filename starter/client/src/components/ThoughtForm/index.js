9da3c52e0953fc96c0f2b2803d5018b317089852
GabrielWarner <gabe0412@uw.edu>
2022-09-23 19:49:43 -0700

    added file upload to form

--- starter/client/src/components/ThoughtForm/index.js
+++ starter/client/src/components/ThoughtForm/index.js
@@ -41,8 +41,6 @@ const ThoughtForm = () => {
     event.preventDefault();
     const data = new FormData();
     data.append('image', fileInput.current.files[0]);
-    // send image file to endpoint with the postImage function
-    // ...
     const postImage = async () => {
       try {
         const res = await fetch('/api/image-upload', {
@@ -53,7 +51,7 @@ const ThoughtForm = () => {
         if (!res.ok) throw new Error(res.statusText);
         const postResponse = await res.json();
         setFormState({ ...formState, image: postResponse.Location });
-    
+        console.log('postImage: ', postResponse.Location);
         return postResponse.Location;
       } catch (error) {
         console.log(error);
